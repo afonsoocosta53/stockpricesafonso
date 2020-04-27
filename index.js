34dd9d1d64923980cfd3767043bae6d08fa6ea9c
afonsoocosta53 <afonso_costa53@hotmail.com>
2020-04-27 12:56:56 +0100

    change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const rp = require("request-promise");
 const $ = require("cheerio");
 const cors = require("cors");
